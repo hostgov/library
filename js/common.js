e4a036c2039ce969f3793e53727346e87486d38f
ryan.q <zmryanq@gmail.com>
2022-09-19 20:51:36 +1000

    19/09/2022

--- js/common.js
+++ js/common.js
@@ -13,9 +13,9 @@ const changeBtnLoginLogout = (type) => {
 
 const logout = () => {
     if (window.confirm("Please confirm you want to log out")) {
-        axios.get("../php/logout.php").then(
+        axios.get("/library/php/logout.php").then(
             response => {
-                localStorage.removeItem("login_member")
+                // localStorage.removeItem("login_member")
                 changeBtnLoginLogout("logout")
                 alert("logout successfully", "success")
             },
@@ -32,7 +32,7 @@ const logout = () => {
 
 const checkLoginBack = () => {
     axios({
-        url: './php/authentication.php',
+        url: '/library/php/authentication.php',
         method: 'POST'
     }).then(
         response => {
@@ -40,6 +40,7 @@ const checkLoginBack = () => {
                 changeBtnLoginLogout("login")
             } else {
                 changeBtnLoginLogout("logout")
+                localStorage.clear()
             }
         },
         error => {
@@ -47,44 +48,7 @@ const checkLoginBack = () => {
         }
     )
 }
-const checkLoginFront = () => {
-    if (localStorage.getItem("login_member")) {
-        if (window.confirm("You have already login, do you want to log out ?") ) {
-            logout()
-        } else {
-            location.href = "/library/index.html"
-        }
-    }
-}
-async function  checkAdmin ()  {
-    await axios({
-        url: '../php/authentication.php',
-        method: 'POST'
-    }).then(
-        response => {
-            if (response.data.code === "3") {
-                changeBtnLoginLogout("login")
-                return true
-            } else {
-                alert('You are not login or login without admin role, redirect to index page', 'danger')
-                // setInterval(() => {
-                //     location.href= "/library/index.html"
-                // }, 2000)
-                return false
-                // location.href= "/library/index.html"
-            }
 
-        },
-        error => {
-            alert(error.message, 'danger')
-            // setInterval(() => {
-            //     location.href= "/library/index.html"
-            // }, 2000)
-            return false
-            // location.href= "/library/index.html"
-        }
-    )
-}
 
 const logoutBtn = document.getElementById("logoutBtn")
 logoutBtn.addEventListener("click", e => {
